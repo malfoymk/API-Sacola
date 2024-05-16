c43edd1b4a5fdab8431732d0a56f8ef0a7f4b5ef
Patrick Lopes <lopes.patrick4@gmail.com>
2024-05-16 12:27:25 -0300

    Feat: Add Files

---
+++ sacola/src/test/java/sacola/example/sacola/SacolaApplicationTests.java
@@ -0,0 +1,13 @@
+package sacola.example.sacola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
