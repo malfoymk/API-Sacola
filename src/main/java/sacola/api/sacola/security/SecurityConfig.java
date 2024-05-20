baf5cc3bb091907a345f8df9a99ccf02b825b36c
Patrick Lopes <lopes.patrick4@gmail.com>
2024-05-20 12:58:01 -0300

    chore: Update SecurityConfig to disable CSRF and permit access to Swagger UI and API docs

--- src/main/java/sacola/api/sacola/security/SecurityConfig.java
+++ src/main/java/sacola/api/sacola/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package sacola.api.sacola.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,11 +11,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeHttpRequests((authz) -> authz
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated()
-            );
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authz) -> authz
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated()
+                );
         return http.build();
     }
 }
