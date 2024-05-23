1ff1044a9eb307ffef767a6b7245c2e3ec9202a6
Patrick Lopes <lopes.patrick4@gmail.com>
2024-05-23 09:49:52 -0300

    Bug

---
+++ src/main/java/sacola/api/sacola/model/Item.java
@@ -0,0 +1,32 @@
+package sacola.api.sacola.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private Product product;
+    private Integer quantidade;
+    @ManyToOne
+    @JsonIgnore
+    private Sacola sacola;
+}
